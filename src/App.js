9de4e079189691cf5f02b3ea0d0a1334e40851e7
drubaby <peryton6@gmail.com>
2019-04-09 07:18:31 -0400

    Separate forecast objs by day

--- src/App.js
+++ src/App.js
@@ -26,15 +26,67 @@ class App extends Component {
     const city = "Detroit";
     // const country = e.target.country.value;
     const country = "US";
-    const cnt = 5
     const API_KEY = `${process.env.REACT_APP_OPEN_WEATHER_MAP_APP_API_KEY}`;
 
     const temp_api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast/daily?q=${city},${country}&cnt=${cnt}`
+      `http://api.openweathermap.org/data/2.5/forecast/?q=${city},${country}&APPID=${API_KEY}&units=imperial`
     );
+
     const response = await temp_api_call.json();
 
-    // debugger
+    const list_obj = response.list;
+    console.log("list_obj: ", list_obj);
+
+    // Problem: response includes array of 40 forecast objects in .list
+    // response.list[0] is current conditions
+    // need to loop through array and create 5 new arrays of each day's forecast
+    // list object includes:
+    // dt: 1554757200
+    // dt_txt: "2019-04-08 21:00:00"
+
+    // One solution: just grab forecasts for 24 hrs in future e.g:
+    // today: list_obj[0], tomorrow: list_obj[9]
+    // let now = new Date()
+    // Tue Apr 09 2019 05:44:18 GMT-0400 (Eastern Daylight Time)
+    // let today = now.getDate()
+    // 9
+    // make date obj
+    // newObj = new Date(dt_txt)
+    // "2019-04-08 21:00:00"
+    // newObj.getDate()
+    // 8
+    const today_date_obj = new Date(list_obj[0].dt_txt);
+    const today_date_num = today_date_obj.getDate();
+
+    let day0_forecasts = [];
+    let day1_forecasts = [];
+    let day2_forecasts = [];
+    let day3_forecasts = [];
+    let day4_forecasts = [];
+
+    for (const forecast of list_obj) {
+      // debugger
+      const day_obj = new Date(forecast.dt_txt);
+      const forecast_date = day_obj.getDate()
+      console.log("For Of loop forecast_date: ", forecast_date);
+      // console.log('today_date_num === ')
+      if (forecast_date === today_date_num) {
+        day0_forecasts.push(forecast);
+      } else if (forecast_date === today_date_num + 1) {
+        day1_forecasts.push(forecast)
+      } else if (forecast_date === today_date_num + 2) {
+        day2_forecasts.push(forecast)
+      } else if (forecast_date === today_date_num + 3) {
+        day3_forecasts.push(forecast)
+      } else if (forecast_date === today_date_num + 4) {
+        day4_forecasts.push(forecast)
+      }
+    }
+    console.log("day0 forecasts: ", day0_forecasts);
+    console.log("day1 forecasts: ", day1_forecasts);
+    console.log("day2 forecasts: ", day2_forecasts);
+    console.log("day3 forecasts: ", day3_forecasts);
+    console.log("day4 forecasts: ", day4_forecasts);
 
     if (city && country) {
       this.setState({
