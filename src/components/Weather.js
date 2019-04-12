4a9014c8d01962588c936f42b5d91ce5cd49c236
drubaby <peryton6@gmail.com>
2019-04-12 11:34:34 -0400

    Format fix on current weather conditions

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const Weather = (props) => {
   // console.log("Weather component props: ", props)
+
+  const date = new Date(props.date * 1000)
+  const displayDate = date.toLocaleDateString("en-US", {weekday: 'long', month: 'long', day: 'numeric'})
+
+
   return(
     <div>
-      {props.country && props.city && <p>Location: {props.city},    {props.country}</p>}
+      {props.city && <h2>Current Conditions</h2>}
+      {props.date && <p>{displayDate}</p>}
+      {props.country && props.city && <p>{props.city},    {props.country}</p>}
       {props.temperature && <p>Temperature: {props.temperature}F</p>}
       {props.humidity && <p>Humidity: {props.humidity}%</p>}
       {props.description && <p>Conditions:  {props.description}</p>}
